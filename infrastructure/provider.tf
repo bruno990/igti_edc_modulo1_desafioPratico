a071a4f4c9618a891d060bf467d69d815d7d11e7
BRUNO MENDONCA <br-romero@hotmail.com>
2021-10-12 11:20:42 -0300

    add esteira deploy

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,18 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 1.0.4"
-    }
-  }
   backend "s3" {
     bucket = "igti-edc-tf-state"
-    key    = "path/to/my/key"
+    key    = "state/igti/edc/mod1/terraform.tfstate"
     region = "us-east-2"
-    
   }
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "${var.aws_region}"
 }
